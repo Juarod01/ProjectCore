445931a8c626cfe8d209aa0b52397a71f3fce4cc
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-05-24 01:34:10 -0500

    Agregar archivos de proyecto.

---
+++ Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectCore.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SenderGridUser { get; set; }
+        public string SenderGridKey { get; set; }
+    }
+}
